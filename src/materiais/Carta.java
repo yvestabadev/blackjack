02b1e97050b83579b040343ca360890adcc8b4cf
Yves Taba <117912315+nossbank@users.noreply.github.com>
2023-04-24 20:40:52 -0300

    adicionado regras do jogo

--- src/materiais/Carta.java
+++ src/materiais/Carta.java
@@ -1,4 +1,10 @@
 package materiais;
 
+import java.util.List;
+
 public record Carta(Naipe naipe, ValorCarta valor) {
+
+    public List<Integer> getValores(){
+        return valor.getValores();
+    }
 }
